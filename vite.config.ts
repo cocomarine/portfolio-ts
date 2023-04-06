d62f9b497b30934201b81b2aa7975603bbcc6ed6
HJ Kang <hkang07@gmail.com>
2023-04-06 21:25:19 +0100

    tsconfig and vite config updated with path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; // comes from types/node that we installed
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // whenever @ occurs, it will be replaced with current directory folder with src added to it
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
+  }
 })
